607bb3e89ad80b0f14369e6915c6ee75e08c2fb4
harshitkumar712 <harshitkumar712@gmail.com>
2020-09-19 19:06:43 +0530

    navbar changes by H

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,33 +1,40 @@
-import React from 'react';
-import '../Style/Header.css'
-import NavigatorButton from './NavigatorButton';
-import menu from "../assets/menu_emoji_n.jpg"
+import React from "react";
+import "../Style/Header.css";
+import NavigatorButton from "./NavigatorButton";
+import menu from "../assets/menu_emoji_n.jpg";
 
 const header = (props) => {
-    return(
+    return (
         // <div>
-            <nav>                
-                <div className="row nav">
-                    <div className="col-sm-2">
-                        <div className="box small">
-                            <img src={menu} alt="Cloudy Sky"></img>
-                        </div>
-                        {/* <img style={{height:"50%", width:"50%", padding:"0px", cursor:"pointer",}} src={menu} alt="boohoo" className="img-responsive"/> */}
+        <nav>
+            <div className="row nav">
+                <div className="col-sm-2">
+                    <div className="box small">
+                        <img src={menu} alt="Cloudy Sky"></img>
                     </div>
-                    <div className="row col-sm-10 navbutton">
-                        <NavigatorButton name="Home" />
-                        <NavigatorButton name="Charts" click={props.chartsClick} region="Ghaziabad"/>
-                        <NavigatorButton name="Add Game"/>
-                        <NavigatorButton name="Leaks"/>
-                        <NavigatorButton name="About Us" click={props.aboutClick}/>
-                        <NavigatorButton name=""/>
-                        {/* <NavigatorButton name=""/><NavigatorButton name=""/><NavigatorButton name=""/><NavigatorButton name=""/><NavigatorButton name=""/><NavigatorButton name=""/> */}
-                    {/* <div className='burger'><i className='fas fa-bars' ></i></div> */}
-                    </div>
+                    {/* <img style={{height:"50%", width:"50%", padding:"0px", cursor:"pointer",}} src={menu} alt="boohoo" className="img-responsive"/> */}
                 </div>
-             </nav>
+                <div className="row col-sm-10 ">
+                     <div className="nav-row">
+                    <NavigatorButton name="Home" />
+                    <NavigatorButton
+                        name="Charts"
+                        click={props.chartsClick}
+                        region="Ghaziabad"
+                    />
+
+                    <NavigatorButton name="Add Game" />
+                    <NavigatorButton name="Leaks" />
+                    <NavigatorButton name="About Us" click={props.aboutClick} />
+                    <NavigatorButton name="" /></div>
+                    {/* <NavigatorButton name=""/><NavigatorButton name=""/><NavigatorButton name=""/><NavigatorButton name=""/><NavigatorButton name=""/><NavigatorButton name=""/> */}
+                  
+                </div>
+ <div className='toggle' onClick={()=>document.querySelector('.nav-row').classList.toggle('active')}><span className='bar' ></span></div>
+            </div>
+        </nav>
         // </div>
-    )
-}
+    );
+};
 
 export default header;
--- src/Components/NavigatorButton.js
+++ src/Components/NavigatorButton.js
@@ -4,9 +4,12 @@ import '../Style/NavigatorButton.css'
 const NavigatorButton = (props) => {
     return (
         // <button className="col-sm-2" onClick={()=>props.click(props.region)}>
-        	 <button className="col-sm-2" >
+        // 	<div className="nav-button"> <button className="col-sm-2" >
+        //     <b>{props.name}</b>
+        // </button></div>
+        <div className="nav-button"> <button  >
             <b>{props.name}</b>
-        </button>
+        </button></div>
     )
 }
 
